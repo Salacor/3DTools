9260a142dae0487de8c1da1a64bf8895fccb212b
Amin <eisam.e.amin@gmail.com>
2025-02-19 16:18:41 +0100

    Solution setup.

---
+++ 3DSolutions/3DSolutions/3DSolutions.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <printerTools.h>
+
+int main()
+{
+    std::cout << "Hello World!" << std::endl;
+
+    printHello();
+}
---
+++ 3DSolutions/3DTools/printerTools.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <iostream>
+
+extern "C" void printHello()
+{
+	std::cout << "Hello World from the 3DTools dll" << std::endl;
+}
